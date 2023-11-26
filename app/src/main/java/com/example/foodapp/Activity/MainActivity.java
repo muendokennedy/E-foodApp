6e1d95dff88c27090c0451e12848c0479821e420
muendokennedy <kennedymunyao999@gmail.com>
2023-11-26 21:00:30 +0300

    Created the foodDomain class

--- app/src/main/java/com/example/foodapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/foodapp/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.foodapp;
+package com.example.foodapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.foodapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
